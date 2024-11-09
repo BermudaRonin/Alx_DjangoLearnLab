4a33c0c27e15957a74bfb701ac91ecb244d39a3f
Bermuda Ronin <bermudaronin@gmail.com>
2024-11-09 23:50:31 +0100

    :recycle: Fix query_librarian_for_library

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,10 +1,4 @@
-# Prepare a Python script query_samples.py in the relationship_app directory. This script should contain the query for each of the following of relationship:
-# Query all books by a specific author.
-# List all books in a library.
-# Retrieve the librarian for a library.
-
 from relationship_app.models import Book, Author, Library
-
 
 def query_all_books_by_author(author_name):
     author = Author.objects.get(name=author_name)
@@ -19,5 +13,5 @@ def query_all_books_in_library(library_name):
 
 
 def query_librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.librarian
+    librarian = Library.objects.get(name=library_name)
+    return librarian
