59d963c5ef2a9cba8bb85835ce20cd5604c107f4
Bermuda Ronin <bermudaronin@gmail.com>
2024-11-10 02:45:34 +0100

    :sparkles: Add Authorizations functionality

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
 from . import views
+from .views import list_books
+from .views import admin_view, librarian_view, member_view
+from .views import add_book, edit_book, delete_book
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path("", list_books, name="home"),
-    path("", LibraryDetailView, name="home"),
-    path("register/", views.register, name="register"),
-    path("login/", LoginView.as_view(template_name="login.html"), name="login"),
-    path("logout/", LogoutView.as_view(template_name="logout.html"), name="logout"),
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+    path('admin-view/', admin_view, name='admin_view'),
+    path('librarian-view/', librarian_view, name='librarian_view'),
+    path('member-view/', member_view, name='member_view'),
+    path('book/add_book/', add_book, name='add_book'),
+    path('book/<int:pk>/edit_book/', edit_book, name='edit_book'),
+    path('book/<int:pk>/delete/', delete_book, name='delete_book'),
 ]
