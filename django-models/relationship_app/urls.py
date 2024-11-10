769de5478f40bbb5808cc7fc28a655b4a0b5845c
Bermuda Ronin <bermudaronin@gmail.com>
2024-11-10 02:24:27 +0100

    :sparkles: Add Auth functionality

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path("", list_books, name="home"),
     path("", LibraryDetailView, name="home"),
+    path("register/", views.register, name="register"),
+    path("login/", LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="logout.html"), name="logout"),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+
 
 def list_books(request):
     books = Book.objects.all()
     context = {"books": books}
     return render(request, "relationship_app/list_books.html", context)
+
 
 class LibraryDetailView(DetailView):
     model = Library
@@ -14,5 +19,34 @@ class LibraryDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        book = self.get_object() 
-        context['average_rating'] = book.get_average_rating()
+        book = self.get_object()
+        context["average_rating"] = book.get_average_rating()
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+    return render(request, "register.html", {"form": form})
+
+
+def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = AuthenticationForm()
+    return render(request, "login.html", {"form": form})
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect("login")
