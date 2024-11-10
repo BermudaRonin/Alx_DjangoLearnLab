acadd228282cbfc240b1642e08b47e78c4d2e1d0
Bermuda Ronin <bermudaronin@gmail.com>
2024-11-10 01:46:43 +0100

    :recycle: Fix templates path

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -6,7 +6,7 @@ from .models import Book, Library
 def list_all_books(request):
     books = Book.objects.all()
     context = {"books": books}
-    return render(request, "list_books.html", context)
+    return render(request, "relationship_app/list_books.html", context)
 
 class LibraryDetailsView(DetailView):
     model = Library
