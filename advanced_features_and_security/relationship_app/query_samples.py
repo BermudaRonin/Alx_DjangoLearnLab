d6fb2b68c81bf2e27a320a11903d7417ab6a9995
Bermuda Ronin <bermudaronin@gmail.com>
2024-11-11 01:45:31 +0100

    :tada: Init advanced_features_and_security dir

---
+++ advanced_features_and_security/relationship_app/query_samples.py
@@ -0,0 +1,17 @@
+from relationship_app.models import Book, Author, Library, Librarian
+
+def query_all_books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    books = Book.objects.filter(author=author)
+    return books
+
+
+def query_all_books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    books = library.books.all()
+    return books
+
+
+def query_librarian_for_library(library_name):
+    librarian = Librarian.objects.get(library=library_name)
+    return librarian
