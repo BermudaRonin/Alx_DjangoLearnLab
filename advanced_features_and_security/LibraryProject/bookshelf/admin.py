c24da3afe11af6ca43b16cfb2d855b4c348eac64
Bermuda Ronin <bermudaronin@gmail.com>
2024-11-11 02:56:39 +0100

    :tada: Init advanced_features_and_security dir

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,44 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
 
 class BookAdmin(admin.ModelAdmin):
     list_filter = ("title", "author", "publication_year")
     search_fields = ("title", "author")
 
+
 admin.site.register(Book, BookAdmin)
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("username", "email", "date_of_birth", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active", "date_of_birth")
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        ("Personal info", {"fields": ("date_of_birth", "profile_photo")}),
+        ("Permissions", {"fields": ("is_staff", "is_active")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "email",
+                    "password1",
+                    "password2",
+                    "date_of_birth",
+                    "profile_photo",
+                ),
+            },
+        ),
+    )
+    search_fields = ("username", "email", "date_of_birth")
+    ordering = ("username",)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,7 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
 
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     publication_year = models.IntegerField()
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password, date_of_birth, profile_photo=None):
+        if not username:
+            raise ValueError('Users must have a username')
+        if not email:
+            raise ValueError('Users must have an email address')
+        if not date_of_birth:
+            raise ValueError('Users must have a date of birth')
+
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+            date_of_birth=date_of_birth,
+            profile_photo=profile_photo,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password, date_of_birth, profile_photo=None):
+        user = self.create_user(
+            username=username,
+            email=email,
+            password=password,
+            date_of_birth=date_of_birth,
+            profile_photo=profile_photo,
+        )
+        user.is_admin = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+    
