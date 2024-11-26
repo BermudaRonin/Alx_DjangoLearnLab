c178683fc624b64b39544129e57c6d8e33da6421
Bermuda Ronin <bermudaronin@gmail.com>
2024-11-26 22:45:49 +0100

    Add access token functionality

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BookList, BookViewSet
+from .views import BookList, BookViewSet, CustomAuthToken
 
 router = DefaultRouter()
 router.register(r"books_all", BookViewSet, basename="book_all")
 
 urlpatterns = [
     path("", include(router.urls)),
-    # path("books/", BookList.as_view(), name="book-list"),
+    path("books/", BookList.as_view(), name="book-list"),
+    path('auth/token/', CustomAuthToken.as_view(), name='auth-token'),  # Token retrieval
 ]
--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api'
+    'rest_framework.authtoken',
+    'api',
+    'rest'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,18 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# REST_FRAMEWORK settings:
+# - TokenAuthentication is used to authenticate users via tokens.
+# - IsAuthenticated permission ensures that only authenticated users can access the API.
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',  # Optional, for session-based auth
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',  # Default permission
+    ],
+}
